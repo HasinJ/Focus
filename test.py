94453c590c5c2d401a7ee9e13f1d77b38ac4d0e1
HasinJ <hasinchoudhuryj@gmail.com>
2021-01-10 17:16:20 -0500

    clicking and pressing tests

--- test.py
+++ test.py
@@ -1,27 +1,40 @@
 # Code to check if left or right mouse buttons were pressed
 import win32api
 import time
+import pynput.keyboard as keyboard
+import pynput.mouse as mouse
 
-state_left = win32api.GetKeyState(0x01)  # Left button down = 0 or 1. Button up = -127 or -128
-state_right = win32api.GetKeyState(0x02)  # Right button down = 0 or 1. Button up = -127 or -128
+
+def on_press(key):
+    print('{0} pressed'.format(
+        key))
+
+def on_release(key):
+    print('{0} release'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+def on_move(x, y):
+    print('Pointer moved to {0}'.format(
+        (x, y)))
+
+def on_click(x, y, button, pressed):
+    print('{0} at {1}'.format(
+        'Pressed' if pressed else 'Released',
+        (x, y)))
+
+def on_scroll(x, y, dx, dy):
+    print('Scrolled {0}'.format(
+        (x, y)))
+
 
 while True:
-    a = win32api.GetKeyState(0x01)
-    b = win32api.GetKeyState(0x02)
+    # Collect events until released
+    with keyboard.Listener(on_release=on_release) as k_listener, mouse.Listener(on_click=on_click) as m_listener:
+        k_listener.join()
+        m_listener.join()
 
-    if a != state_left:  # Button state changed
-        state_left = a
-        print(a)
-        if a < 0:
-            print('Left Button Pressed')
-        else:
-            print('Left Button Released')
 
-    if b != state_right:  # Button state changed
-        state_right = b
-        print(b)
-        if b < 0:
-            print('Right Button Pressed')
-        else:
-            print('Right Button Released')
     time.sleep(0.001)
