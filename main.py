481f59f6a91782c02b6ff6df1e0d3b84e741db84
HasinJ <hasinchoudhuryj@gmail.com>
2021-01-15 13:46:53 -0500

    checkbox time between alerts

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Main():
     def __init__(self, parent):
         self.parent=parent
         self.timeBetween=IntVar()
+        self.alert=BooleanVar()
         self.error=0
         self.choices=['Hours','Seconds','Minutes','Hours']
         self.choice=StringVar(parent)
@@ -15,6 +16,7 @@ class Main():
         #pads so that everything is spaced evenly
         padx=30
         pady=30
+        textframe=0
 
         self.mainFrame = Frame(self.parent)
 
@@ -23,15 +25,18 @@ class Main():
         Button(self.mainFrame,text='Start',command=self.start).pack(padx=padx,pady=1)
         frame = Frame(self.mainFrame) #frame within main frame
 
-        #time-in-between-alerts label
-        Label(frame,text = 'Time in between alerts: ').grid(sticky='W')
 
         #time-in-between-alerts text input box
         textframe = Frame(frame, width=100, height=50)
         textframe.columnconfigure(0, weight=10)
         textframe.grid_propagate(False)
+
         Entry(textframe,textvariable = self.timeBetween,width=80).grid(sticky="we",pady=15)
         self.timeBetween.set(61)
+
+        #time-in-between-alerts label
+        Checkbutton(frame, text='Time in between alerts: ',variable=self.alert, onvalue=1, offvalue=0, command = lambda : self.toggle(textframe)).grid(sticky='W')
+        Label(frame,text = '').grid(sticky='W')
 
         #time-in-between-alerts text options
         OptionMenu(frame, self.choice, *self.choices).grid(row=0,column=2,padx=15,pady=10)
@@ -41,6 +46,13 @@ class Main():
         textframe.grid(row=0,column=1,padx=padx,pady=10)
         frame.pack(padx=padx,pady=pady)
         self.mainFrame.pack()
+        self.toggle(textframe)
+
+    def toggle(self,frame):
+        if self.alert.get()==1:
+            for child in frame.winfo_children(): child.configure(state='enable')
+        if self.alert.get()==0:
+            for child in frame.winfo_children(): child.configure(state='disable')
 
     def start(self):
         timer=self.timeBetween.get()
@@ -65,7 +77,6 @@ class Application(Frame):
 
     def end(self):
         print("hello")
-
 
 
 if __name__=="__main__":
